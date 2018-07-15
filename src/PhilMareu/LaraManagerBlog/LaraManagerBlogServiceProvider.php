2727a852d0b96f1adc41b03246e190076debee84
Phil Mareu <philmareu@gmail.com>
2018-07-15 15:49:55 -0500

    initial commit

---
+++ src/PhilMareu/LaraManagerBlog/LaraManagerBlogServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PhilMareu\LaraManagerBlog;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraManagerBlogServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}

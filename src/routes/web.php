2a413f735cdb9f88dc7aa8d1262272aaacb92773
Phil Mareu <philmareu@gmail.com>
2018-07-16 12:19:41 -0500

    updated namespace

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-Route::group(['namespace' => 'PhilMareu\LaraManagerBlog\Http\Controllers', 'middleware' => 'web'], function()
+Route::group(['namespace' => substr(app()->getNamespace(), 0, -1), 'middleware' => 'web'], function()
 {
-    Route::get('blog', 'BlogController@index');
-    Route::get('blog/{year}/{month}/{day}/{slug}', 'BlogController@show');
+    Route::get('blog', 'Http\Controllers\BlogController@index');
+    Route::get('blog/{year}/{month}/{day}/{slug}', 'Http\Controllers\BlogController@show');
 });

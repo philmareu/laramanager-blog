5042a9ede2fa1eff5df6244b5710628c6051db91
Phil Mareu <philmareu@gmail.com>
2018-07-16 08:27:13 -0500

    added routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,9 +7,4 @@ Route::group(['namespace' => 'PhilMareu\LaraManagerBlog\Http\Controllers', 'midd
     Route::get('blog/{year}/{month}/{day}/{slug}', 'BlogController@show');
     Route::get('blog/category/{category}', 'BlogController@category');
     Route::get('blog/archive/{year}/{month}', 'BlogController@archive');
-
-    Route::group(['prefix' => 'admin', 'middleware' => 'admin', 'as' => 'admin.'], function()
-    {
-        Route::get('blog', 'Admin/BlogController@index');
-    });
 });

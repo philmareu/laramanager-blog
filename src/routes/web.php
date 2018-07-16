4ee19dc0bf0acdacb8ef7ba9f318381a781fdb96
Phil Mareu <philmareu@gmail.com>
2018-07-16 11:44:03 -0500

    keeping routes simple

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,8 +3,5 @@
 Route::group(['namespace' => 'PhilMareu\LaraManagerBlog\Http\Controllers', 'middleware' => 'web'], function()
 {
     Route::get('blog', 'BlogController@index');
-    Route::get('blog/preview/{id}', 'BlogController@preview')->middleware('admin');
     Route::get('blog/{year}/{month}/{day}/{slug}', 'BlogController@show');
-    Route::get('blog/category/{category}', 'BlogController@category');
-    Route::get('blog/archive/{year}/{month}', 'BlogController@archive');
 });

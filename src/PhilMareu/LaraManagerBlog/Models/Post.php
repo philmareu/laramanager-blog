f698f99541f810afb11f2a84d0713709faf4f9a1
Phil Mareu <philmareu@gmail.com>
2021-09-10 11:04:08 -0500

    make post model objectable

--- src/PhilMareu/LaraManagerBlog/Models/Post.php
+++ src/PhilMareu/LaraManagerBlog/Models/Post.php
@@ -3,9 +3,12 @@
 namespace PhilMareu\LaramanagerBlog\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use PhilMareu\Laramanager\Database\Objectable;
 
 class Post extends Model
 {
+    use Objectable;
+
     protected $dates = ['published_at'];
 
     public function author()
